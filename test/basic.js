3d8de994051d586ca64ae1bd2ea81996ff43125e
Andreas Lind <andreas@one.com>
2016-07-22 17:38:45 +0200

    Tests: Switch to Unexpected.

--- test/basic.js
+++ test/basic.js
@@ -1,23 +1,22 @@
-var assert = require('chai').assert,
+var expect = require('unexpected'),
     p = require('../');
 
 describe('Basic', function () {
     it('Single parameter returns templating function', function () {
         var t = p('proto://{username}:pwd@{sub}.domain.tld');
-        assert.isFunction(t);
-        assert.lengthOf(t, 1);
+        expect(t, 'to be a function').and('to have arity', 1);
 
         // Fill the tempalte
-        var d = t({username: '123', sub: '456'});
-        assert.isObject(d);
-        assert.propertyVal(d, 'auth', '123:pwd');
-        assert.propertyVal(d, 'host', '456.domain.tld');
-        assert.propertyVal(d, 'hostname', '456.domain.tld');
-        assert.propertyVal(d, 'href', 'proto://123:pwd@456.domain.tld');
-        assert.propertyVal(d, 'password', 'pwd');
-        assert.propertyVal(d, 'protocol', 'proto:');
-        assert.propertyVal(d, 'slashes', true);
-        assert.propertyVal(d, 'username', '123');
+        expect(t({username: '123', sub: '456'}), 'to satisfy', {
+            auth: '123:pwd',
+            host: '456.domain.tld',
+            hostname: '456.domain.tld',
+            href: 'proto://123:pwd@456.domain.tld',
+            password: 'pwd',
+            protocol: 'proto:',
+            slashes: true,
+            username: '123'
+        });
     });
 
     it('Two parameters invokes it directly', function () {
@@ -26,59 +25,65 @@ describe('Basic', function () {
             {username: '123', sub: '456'}
         );
 
-        assert.isObject(d);
-        assert.propertyVal(d, 'auth', '123:pwd');
-        assert.propertyVal(d, 'host', '456.domain.tld');
-        assert.propertyVal(d, 'hostname', '456.domain.tld');
-        assert.propertyVal(d, 'href', 'proto://123:pwd@456.domain.tld');
-        assert.propertyVal(d, 'password', 'pwd');
-        assert.propertyVal(d, 'protocol', 'proto:');
-        assert.propertyVal(d, 'slashes', true);
-        assert.propertyVal(d, 'username', '123');
+        expect(d, 'to satisfy', {
+            auth: '123:pwd',
+            host: '456.domain.tld',
+            hostname: '456.domain.tld',
+            href: 'proto://123:pwd@456.domain.tld',
+            password: 'pwd',
+            protocol: 'proto:',
+            slashes: true,
+            username: '123'
+        });
     });
 });
 
 it('should populate the username field even when there is no password', function () {
-    var out = p('smtp://{username}@foo.com/', {username: 'theuser'});
-    assert.propertyVal(out, 'auth', 'theuser');
-    assert.propertyVal(out, 'username', 'theuser');
+    expect(p('smtp://{username}@foo.com/', {username: 'theuser'}), 'to satisfy', {
+        auth: 'theuser',
+        username: 'theuser'
+    });
 });
 
 it('should populate the password field with an empty string when there is a colon before the @', function () {
-    var out = p('smtp://{username}:@foo.com/', {username: 'theuser'});
-    assert.propertyVal(out, 'auth', 'theuser:');
-    assert.propertyVal(out, 'username', 'theuser');
-    assert.propertyVal(out, 'password', '');
+    expect(p('smtp://{username}:@foo.com/', {username: 'theuser'}), 'to satisfy', {
+        auth: 'theuser:',
+        username: 'theuser',
+        password: ''
+    });
 });
 
 describe('Colons in usernames', function () {
     var tpl = p('proto://{u}:{p}@domain.tld/foobar/');
     it('Can handle : in username', function () {
-        var out = tpl({u: 'user:name', p: 'pwd'});
-        assert.propertyVal(out, 'auth', 'user:name:pwd');
-        assert.propertyVal(out, 'username', 'user:name');
-        assert.propertyVal(out, 'password', 'pwd');
+        expect(tpl({u: 'user:name', p: 'pwd'}), 'to satisfy', {
+            auth: 'user:name:pwd',
+            username: 'user:name',
+            password: 'pwd'
+        });
     });
 
     it('Can handle : in password', function () {
-        var out = tpl({u: 'u', p: 'pass:word'});
-        assert.propertyVal(out, 'auth', 'u:pass:word');
-        assert.propertyVal(out, 'username', 'u');
-        assert.propertyVal(out, 'password', 'pass:word');
+        expect(tpl({u: 'u', p: 'pass:word'}), 'to satisfy', {
+            auth: 'u:pass:word',
+            username: 'u',
+            password: 'pass:word'
+        });
     });
 
     it('Can handle : in username & password', function () {
-        var out = tpl({u: 'user:name', p: 'pass:word'});
-        assert.propertyVal(out, 'auth', 'user:name:pass:word');
-        assert.propertyVal(out, 'username', 'user:name');
-        assert.propertyVal(out, 'password', 'pass:word');
+        expect(tpl({u: 'user:name', p: 'pass:word'}), 'to satisfy', {
+            auth: 'user:name:pass:word',
+            username: 'user:name',
+            password: 'pass:word'
+        });
     });
 });
 
 describe('does not touch the non-atomic parts of the parsed url', function () {
-    var tpl = p('http://foo.com/{?url}');
     it('leaves chars encoded by url-template alone in href', function () {
-        var out = tpl({url: 'http://bar.com/?quux=baz&blah=blerg'});
-        assert.propertyVal(out, 'href', 'http://foo.com/?url=http%3A%2F%2Fbar.com%2F%3Fquux%3Dbaz%26blah%3Dblerg');
+        expect(p('http://foo.com/{?url}')({url: 'http://bar.com/?quux=baz&blah=blerg'}), 'to satisfy', {
+            href: 'http://foo.com/?url=http%3A%2F%2Fbar.com%2F%3Fquux%3Dbaz%26blah%3Dblerg'
+        });
     });
 });
